d53532281736f2c9020eb50cbf064aef9f6caf26
Ryo-cool <coolboxing576@gmail.com>
2020-09-08 16:34:05 +0900

    バリデーション

--- back/app/models/user.rb
+++ back/app/models/user.rb
@@ -1,4 +1,10 @@
+require "validator/email_validator"
+
 class User < ApplicationRecord
+
+  # バリデーション直前
+  before_validation :downcase_email
+
   # gem bcrypt
   has_secure_password
 
@@ -12,4 +18,29 @@ class User < ApplicationRecord
                           message: :invalid_password
                         },
                         allow_blank: true
+  validates :email, presence: true,
+                    email: { allow_blank: true }
+
+  ## methods
+  # class method  ###########################
+  class << self
+    # emailからアクティブなユーザーを返す
+    def find_activated(email)
+      find_by(email: email, activated: true)
+    end
+  end
+  # class method end #########################
+
+  # 自分以外の同じemailのアクティブなユーザーがいる場合にtrueを返す
+  def email_activated?
+    users = User.where.not(id: id)
+    users.find_activated(email).present?
+  end
+
+  private
+
+    # email小文字化
+    def downcase_email
+      self.email.downcase! if email
+    end
 end
