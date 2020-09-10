7f5f5541fb795e074bda02fe3248e925b50d6a2f
Ryo-cool <coolboxing576@gmail.com>
2020-09-10 16:38:17 +0900

    データの受け取り

--- front/components/user/userFormEmail.vue
+++ front/components/user/userFormEmail.vue
@@ -1,5 +1,6 @@
 <template>
   <v-text-field
+    v-model="setMail"
     label="メールアドレスを入力"
     placeholder="your@email.com"
     outlined
@@ -8,5 +9,17 @@
 
 <script>
 export default {
+  props: {
+    email: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    setMail: {
+      get () { return this.email },
+      set (newVal) { return this.$emit('update:email', newVal) }
+    }
+  }
 }
 </script>
