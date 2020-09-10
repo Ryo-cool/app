d27933a9d0f050f1aa0f4c0fd1e8b4e6e03f6cc5
Ryo-cool <coolboxing576@gmail.com>
2020-09-10 17:20:11 +0900

    入力フォームにバリデーション追加

--- front/components/user/userFormName.vue
+++ front/components/user/userFormName.vue
@@ -1,6 +1,8 @@
 <template>
   <v-text-field
     v-model="setName"
+    :counter="max"
+    :rules="rules"
     label="ユーザー名を入力"
     placeholder="あなたの表示名"
     outlined
@@ -9,12 +11,25 @@
 
 <script>
 export default {
+  // 親からのデータ受け取り(v-model)
   props: {
     name: {
       type: String,
       default: ''
     }
   },
+  // バリデーションはここで設定
+  data () {
+    const max = 30
+    return {
+      max,
+      rules: [
+        v => !!v || '',
+        v => (!!v && max >= v.length) || `${max}文字以内で入力してください`
+      ]
+    }
+  },
+  // 親からのデータをgetで返し、setで編集
   computed: {
     setName: {
       get () { return this.name },
