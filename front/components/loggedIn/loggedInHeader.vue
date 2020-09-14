3bbbdaeec5ff0ab409ab1db9a19803a122bc3a97
Ryo-cool <coolboxing576@gmail.com>
2020-09-14 19:03:58 +0900

    ダッシュボード表示

---
+++ front/components/loggedIn/loggedInHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <logged-in-nav-drawer
+      :drawer.sync="drawer"
+    />
+    <logged-in-app-bar #nav-icon>
+      <v-app-bar-nav-icon
+        @click="drawer = !drawer"
+      />
+    </logged-in-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data () {
+    return {
+      drawer: null
+    }
+  }
+}
+</script>
