8390085dd811ead89ba4574479c5b68475693431
Ryo-cool <coolboxing576@gmail.com>
2021-02-04 16:32:43 +0900

    PokeAPI

--- front/pages/posts.vue
+++ front/pages/posts.vue
@@ -1,42 +1,37 @@
 <template>
   <v-container>
     <v-row align="center" justify="center">
-      <v-col cols="12">
+      <v-col cols="6">
         <v-text-field
-        label="postname"
+        label="ポケモン名"
         v-model="name"
         prepend-icon=""
         type="text"
         />
-        <v-btn color="primary" @click="createPost">ADD post</v-btn>
+        <v-btn color="primary" @click="createPost">ポケモンを検索する</v-btn>
       </v-col>
-      <v-col cols="12">
-          <h1>投稿画面 </h1>
+      <v-col cols="6">
+          <v-card>
+          <v-img
+            :src="image"
+            :aspect-ratio="1/1"
+            max-height="300"
+            contain
+          ></v-img>
+          <v-card-title>{{s.name}}</v-card-title>
+          <v-card-text>身長:{{ height }}m</v-card-text>
+          <v-card-text>重さ:{{ weight }}kg</v-card-text>
+          <v-card-sub-title>ステータス</v-card-sub-title>
+          <v-card-text>HP:{{ hp.base_stat }}</v-card-text>
+          <v-card-text>こうげき:{{ attack.base_stat }}</v-card-text>
+          <v-card-text>ぼうぎょ:{{ defense.base_stat }}</v-card-text>
+          <v-card-text>とくこう:{{ specialAttack.base_stat }}</v-card-text>
+          <v-card-text>とくぼう:{{ specialDefense.base_stat }}</v-card-text>
+          <v-card-text>すばやさ:{{ speed.base_stat }}</v-card-text>
+          </v-card>
+
         </v-col>
       </v-row>
-
-      <v-card
-        class="mx-auto"
-        max-width="300"
-        tile
-      >
-          <v-list rounded>
-            <v-subheader>posts</v-subheader>
-            <v-list-item-group color="primary">
-              <v-list-item
-                v-for="post in posts"
-                :key="posts.id"
-                @click=""
-              >
-                <v-list-item-content>
-                  <v-list-item-title v-text="post.name"></v-list-item-title>
-                  <v-list-item-title v-text="post.text"></v-list-item-title>
-                  <v-list-item-title v-text="post.id"></v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-            </v-list-item-group>
-          </v-list>
-      </v-card>
     </v-container>
 </template>
 
@@ -47,25 +42,35 @@ export default {
   data() {
     return {
       name: "",
-      text: "",
       msgs: [],
-      posts: []
+      posts: [],
+      s:[],
+      image:[],
+      height:[],
+      weight:[],
+      hp:[],
+      attack:[],
+      defense:[],
+      specialAttack:[],
+      specialDefense:[],
+      speed: []
     }
-  },
-  created() {
-    // ユーザーをaxiosで取得
-    axios.get("/api/v1/posts").then(res => {
-      if (res.data) {
-          this.posts = res.data
-          }
-        })
   },
   methods: {
     // ユーザーをaxiosで登録
     createPost(){
-      axios.post("/api/v1/posts", {name: this.name,text: this.text}).then(res => {
+      axios.get(`https://pokeapi.co/api/v2/pokemon/${this.name}`).then(res => {
         if (res.data) {
-            this.posts.push(res.data)
+            this.s = res.data
+            this.height = res.data.height / 10
+            this.weight = res.data.weight /10
+            this.image = res.data.sprites.front_default
+            this.hp = res.data.stats[0]
+            this.attack = res.data.stats[1]
+            this.defense = res.data.stats[2]
+            this.specialAttack = res.data.stats[3]
+            this.specialDefense = res.data.stats[4]
+            this.speed = res.data.stats[4]
         }
       })
     }
